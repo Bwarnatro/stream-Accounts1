08910653dd5eed54fdc051c99e74cdc22f90f692
Bwarnatro <89459134+Bwarnatro@users.noreply.github.com>
2021-08-31 12:44:56 +0300

    Update Corona.js

--- Corona.js
+++ Corona.js
@@ -1,20 +1,72 @@
+
 const Discord = require("discord.js");
+
 const Yaxi = new Discord.Client();
+
 //////// playing or dnd  streaming ////////
+
 Yaxi.on("ready", () => {
+
   console.log(
+
     `Online In Servers`
+
   );
+
   let statuses = [
+
   
+
   `You only live once, so try to live for God.`,
+
  
 
+  ];
+
+  setInterval(function() {
+
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+
+    corona.user.setActivity(STREAMING, {      
+
+    type: "STREAMING", 
+
+    url: "https://www.twitch.tv/faith"
+
+    });
+
+  }, 2000);
+
+});
+
 ///////////////////////
+
 corona.on("ready", () => {
-    var join = corona.channels.get("874334828831600672"); /// id voice
+
+    var join = corona.channels.get("874334828265365558"); /// id voice
+
     if (join) join.join();
+
   });
-//////////////// tokn lera da bn
-corona.login(""ODgxOTcwNjM1NjkyMTk1ODQx.YS0okg.RvO1EcFpitlagqArA1DBq5FVviE"");
+
+//////////////// tokn lera da bne 
+
+corona.login(""mfa.xGLik_QTZQmtiKaMA_wxKjc33bGmzawG9EjTAGkLG-Y-Hn0RlLAnpZcmvZZxLedrD_Y-Zz7GlxghfD2laK8G"");
+
+
+
+
+
+
+  
+
+ 
+
+
+
+
+
+ 
+
+
 
